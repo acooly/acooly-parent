fc2981c42d74773a024c6489a8dde871e0a7641e
zhangpu <zhangpu@ebe96db4-64c5-0a46-a2e0-819f53ba128a>
2015-11-09 05:45:31 +0000

    修改脚本默认说明
    git-svn-id: http://acooly.cn/svn/framework/acooly-parent/trunk@1031 ebe96db4-64c5-0a46-a2e0-819f53ba128a

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,7 @@ echo [INFO] Install parent pom.xml to maven repository.
 
 cd %~dp0
 set profile=
-set /P profile=maven profile: %=%
+set /P profile=maven profile(def: acooly[yiji_dev,yiji_online]): %=%
 if defined profile (set profile=%profile%) else set profile=acooly
 echo [INFO] Chose maven profile: %profile%
 call mvn clean deploy -Dmaven.test.skip=true -P%profile%
